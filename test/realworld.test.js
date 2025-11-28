78796661bb739e0a713330d9d800cd4828691120
maksimmost <maksim.mostovoj.al@gmail.com>
2025-11-28 16:15:22 +0300

    новые тесты

--- test/realworld.test.js
+++ test/realworld.test.js
@@ -2,25 +2,27 @@ import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
 //Переменные
+let name = faker.person.fullName();
+let email = faker.internet.email({ provider: "qa.guru" });
+let password = faker.internet.password({ length: 10 });
+const url = "https://realworld.qa.guru";
+
+const getRegistration = async (page) => {};
 
 //todo Нейминг теста
 test("Пользователь может зарегистрироваться используя email и пароль", async ({
   page,
 }) => {
-  await page.goto("https://realworld.qa.guru/");
+  await page.goto(url);
   await page.getByRole("link", { name: "Sign up" }).click();
   await page.getByRole("textbox", { name: "Your Name" }).click();
-  await page
-    .getByRole("textbox", { name: "Your Name" })
-    .fill("sniper101120251");
+  await page.getByRole("textbox", { name: "Your Name" }).fill(name);
   await page.getByRole("textbox", { name: "Email" }).click();
-  await page
-    .getByRole("textbox", { name: "Email" })
-    .fill("sniper101120251@ya.ru");
+  await page.getByRole("textbox", { name: "Email" }).fill(email);
   await page.getByRole("textbox", { name: "Password" }).click();
-  await page.getByRole("textbox", { name: "Password" }).fill("12345678");
+  await page.getByRole("textbox", { name: "Password" }).fill(password);
   await page.getByRole("button", { name: "Sign up" }).click();
-  await expect(page.getByRole("navigation")).toContainText("sniper10112025");
+  await expect(page.getByRole("navigation")).toContainText(name);
 });
 
 /*
